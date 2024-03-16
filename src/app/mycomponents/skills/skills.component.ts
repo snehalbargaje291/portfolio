af0c099a91a4e7ee942ca216b0ca487145922fea
snehalbargaje291 <snehal.bargaje291@gmail.com>
2024-03-16 16:41:59 +0530

    skills

--- src/app/mycomponents/skills/skills.component.ts
+++ src/app/mycomponents/skills/skills.component.ts
@@ -9,9 +9,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './skills.component.css'
 })
 export class SkillsComponent {
-  skills: string[] = ['HTML', 'CSS', 'JavaScript', 'Angular', 'React', 'Bootstrap','C', 'C#', 'Java', 'Python', 'MySQL', 'SQL Server','Github' ];
+  skills: string[] = ['icons8-html-48.png', 'icons8-css-48.png', 'icons8-bootstrap-48.png', 'icons8-javascript-48.png', 'icons8-typescript-48.png', 'icons8-angular-48.png', 'icons8-react-40.png','icons8-java-48.png', 'icons8-c-48.png', 'icons8-python-48.png', 'icons8-mysql-48.png', 'icons8-github-48.png', 'icons8-visual-studio-code-2019-48.png', 'icons8-visual-studio-48.png','icons8-figma-48.png' ];
 
-  getRandomColor(): string {
-    return '#' + Math.floor(Math.random() * 16777215).toString(16);
-  }
+  // getRandomColor(): string {
+  //   return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  // }
 }
